5ee84c089f09c437a37f3ffd3b0ed5c751802351
gassc <christian.gass@outlook.com>
2019-01-15 15:13:46 -0500

    config

--- src/custom/content.js
+++ src/custom/content.js
@@ -12,5 +12,5 @@ var fs = require('fs');
 module.exports =
   '# Introduction\n' +
   fs.readFileSync('./content/introduction.md', 'utf8') + '\n' +
-  '# Example\n' +
-  fs.readFileSync('./content/example.md', 'utf8') + '\n';
+  '# Resources\n' +
+  fs.readFileSync('./content/rainfall-teragon.md', 'utf8') + '\n';
